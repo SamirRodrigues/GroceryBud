3d4a9c326f55b00d3ca0a9b9554f0dc1b4191767
SamirRodrigues <emaildesamir@gmail.com>
2021-12-07 17:04:44 -0300

    Base App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
+import { FaBullseye } from 'react-icons/fa'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name
+      }
+      setList([...list, newItem])
+      setName('')
+    }
+  }
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e. g. eggs"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="drocery-container">
+          <List items={list} />
+          <button className="clear-btn">Clear Items</button>
+        </div>
+      )}
+    </section>
+  )
 }
 
 export default App
