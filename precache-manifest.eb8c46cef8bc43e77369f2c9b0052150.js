bc5f5e5973cb1d4782cfe9c60229d8596e49909b
SamirRodrigues <emaildesamir@gmail.com>
2021-12-13 02:18:42 -0300

    Updates

---
+++ precache-manifest.eb8c46cef8bc43e77369f2c9b0052150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29193163a0c28e32c811bfdb4964e83a",
+    "url": "/GroceryBud/index.html"
+  },
+  {
+    "revision": "5534ded6efefb661338c",
+    "url": "/GroceryBud/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/GroceryBud/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GroceryBud/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5534ded6efefb661338c",
+    "url": "/GroceryBud/static/js/main.e845ba76.chunk.js"
+  },
+  {
+    "revision": "532d1b84ab626b7ba23e",
+    "url": "/GroceryBud/static/js/runtime-main.2f8342ed.js"
+  }
+]);
